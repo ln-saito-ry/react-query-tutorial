caab19ed9aba7fcdbe91639ebf20efc7f9f7a98c
saito-ry <saito-ry@landnet.co.jp>
2023-04-11 09:55:43 +0900

    一通りのコンポーネントを実装

---
+++ src/components/Planets.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets/');
+  return res.json();
+};
+
+const Planets = () => {
+  // 第二引数は基本的にasyncにすべき？asyncじゃなくてもOKf
+  const { data, status } = useQuery('planets', fetchPlanets);
+  console.log(data);
+  return (
+    <div>
+      <h2>Planets</h2>
+      {/* <p>{status}</p> */}
+      {status === 'loading' && <div>Loading data...</div>}
+      {status === 'error' && <div>Error fetching data</div>}
+      {status === 'success' && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Planets;
