01a48660f25cb42c4b47631989a1c1353f8c54ad
saito-ry <saito-ry@landnet.co.jp>
2023-04-11 12:18:00 +0900

    pagination実装

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  // 第二引数は基本的にasyncにすべき？asyncじゃなくてもOKf
+  const [page, setPage] = useState(1);
+
+  // 第二引数は基本的にasyncにすべき？asyncじゃなくてもOK
   const { isLoading, error, data, status } = useQuery(
-    ['planets'],
-    fetchPlanets,
+    ['planets', page],
+    // 引数を渡す場合はwrapするかbindを使う
+    () => fetchPlanets(page),
     {
-      staleTime: 0,
-      cacheTime: 1000,
+      staleTime: 1000,
+      cacheTime: 100000,
       // status === successとなった際に実行される
       onSuccess: () => console.log('data fetched'),
       // status === errorとなった際に実行される
       onError: () => console.error('data fetch faild'),
     }
   );
-  console.log(data);
+
   return (
     <div>
       <h2>Planets</h2>
-      {/* <p>{status}</p> */}
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
+
       {status === 'loading' && <div>Loading data...</div>}
       {status === 'error' && <div>Error fetching data</div>}
       {status === 'success' && (
